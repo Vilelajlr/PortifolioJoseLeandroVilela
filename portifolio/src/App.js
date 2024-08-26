3e18eefbf4d166bd3e4ccaa34f568dfde8e4b4c3
José Leandro Vilela <joseleandro2807@gmail.com>
2024-08-26 09:04:24 -0300

    att portifolio

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -4,36 +4,41 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Contact from './components/pages/Contact';
-import Container from './components/layout/Container';
+
 import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+
+
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Container>
+      
         <Navbar />
         <Routes>
           <Route
             path="/"
             element={
-              <div>
+              <div className="max">
                 <div id="home" style={{ minHeight: '100dvh' }}>
                   <Home />
                 </div>
-                <div id="about" style={{ minHeight: '50dvh' }}>
-                  <h1>Empresa Section</h1>
-                  {/* Conteúdo da seção Empresa */}
+                
+                <div id="about">
+                  <About />
                 </div>
                 <div id="projects" style={{ height: '100vh' }}>
                   <h1>Projetos Section</h1>
                   {/* Conteúdo da seção Projetos */}
                 </div>
-              </div>
+                </div> 
             }
           />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Container>
+      
     </Router>
   );
 }
--- portifolio/src/components/layout/Navbar.jsx
+++ portifolio/src/components/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll'; // Importar Link do react-scroll
 import { Link as RouterLink, useNavigate } from 'react-router-dom'; // Importar Link e useNavigate do react-router-dom
 
@@ -6,6 +6,7 @@ import styles from './modules/Navbar.module.css';
 import logo from '../imgs/LogoSomenteDesenho.png';
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false); // Estado para controlar a rolagem
   const navigate = useNavigate(); // Hook para navegação
 
   const handleScroll = (section) => {
@@ -15,41 +16,55 @@ function Navbar() {
     }, 100); // Pequeno delay para garantir que a rolagem ocorra após a navegação
   };
 
+  // Hook para verificar a rolagem da página
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
-  
-    
-    <nav className={styles.navbar}>
-      <div className={styles.menu}>
-        <ScrollLink to="home" smooth={true} duration={500}>
-          <div className={styles.img_logo}>
-            <img src={logo} alt="Logo Costs" />
-          </div>
-        </ScrollLink>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('home'); }}>
-              Home
-            </a>
-          </li>
-          <li className={styles.item}>
-            <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('about'); }}>
-              About
-            </a>
-          </li>
-          <li className={styles.item}>
-            <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('projects'); }}>
-              Projects
-            </a>
-          </li>
-          <li className={styles.item}>
-            <RouterLink to="/contact">
-              Contact
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    
+    <div className={`${scrolled ? styles.scrolled : ''}`}>
+      <nav className={`${styles.navbar} `}>
+        <div className={styles.menu}>
+          <ScrollLink to="home" smooth={true} duration={500}>
+            <div className={styles.img_logo}>
+              <h1>José Leandro Vilela</h1>
+            </div>
+          </ScrollLink>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('home'); }}>
+                Home
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('about'); }}>
+                About
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a href="/" onClick={(e) => { e.preventDefault(); handleScroll('projects'); }}>
+                Projects
+              </a>
+            </li>
+            <li className={styles.item}>
+              <RouterLink to="/contact">
+                Contact
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
